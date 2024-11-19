1be5acf7198cd3d266a7c8f5c551c251837e3519
Matheus Hora <matheus11hora@gmail.com>
2024-11-19 01:11:30 -0300

    ✨ feat: Cria model de location

---
+++ lib/src/models/location_model.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class LocationModel {
+  final int? id;
+  final String zipCode;
+  final String address;
+  final String? lat;
+  final String? lng;
+  final String? number;
+  final String? complement;
+
+  LocationModel({
+    this.id,
+    required this.zipCode,
+    required this.address,
+    required this.lat,
+    required this.lng,
+    this.number,
+    this.complement,
+  });
+
+  factory LocationModel.fromMap(Map<String, dynamic> map) {
+    return LocationModel(
+      id: map['id'] as int,
+      zipCode: map['zipCode'] as String,
+      address: map['address'] as String,
+      lat: map['lat'] as String?,
+      lng: map['lng'] as String?,
+      number: map['number'] as String?,
+      complement: map['complement'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'zipCode': zipCode,
+      'address': address,
+      'number': number,
+      'complement': complement,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory LocationModel.fromJson(String source) =>
+      LocationModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
