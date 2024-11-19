94ecaa57d64635d6cc8971f12e99c55f6a4a064a
Matheus Hora <matheus11hora@gmail.com>
2024-11-19 01:11:43 -0300

    ✨ feat: Cria binding do app

---
+++ lib/src/core/binding/aplication_binding.dart
@@ -0,0 +1,22 @@
+import 'package:anota_cep/src/database/dao/location_dao.dart';
+import 'package:anota_cep/src/database/dao/location_dao_impl.dart';
+import 'package:anota_cep/src/database/sqlite_database.dart';
+import 'package:anota_cep/src/database/sqlite_database_impl.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+import '../network/rest_client.dart';
+
+class AplicationBinding extends ApplicationBindings {
+  @override
+  List<Bind<Object>> bindings() => [
+        Bind.lazySingleton(
+          (i) => RestClient(),
+        ),
+        Bind.lazySingleton<SQLiteDatabase>(
+          (i) => SQLiteDatabaseImpl.instance,
+        ),
+        Bind.lazySingleton<LocationDao>(
+          (i) => LocationDaoImpl(i()),
+        ),
+      ];
+}
